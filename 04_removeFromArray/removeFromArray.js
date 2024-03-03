1f991caece6e9b03c5744b4ab499e188ace3ddf7
KingNoran <kingnoran8@gmail.com>
2024-03-03 21:54:11 +0800

    Finished Js Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+let array = ["Hello", 2, "World", 4, 5];
 
+const removeFromArray = function(array, ...elements) {
+    for (const element of elements){
+        while (array.includes(element)){
+            array.splice(array.indexOf(element), 1);
+        }
+    }
+    return array;
 };
+
+console.log(removeFromArray(array, "World"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number" || start < 0 || end < 0){
+        return "ERROR";
+    }
+    if (start > end){
+        const a = start;
+        start = end;
+        end = a;
+    }
+    // Solve for n (number of terms)
+    const terms = end - (start - 1)
+    // Solve for summation
+    const sum = terms / 2 * ((2 * start) + (terms - 1));
+    return sum;
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
